795e0f39c818867d9f9f21d2aef3a4df81a94db0
uhtetaungkyaw <htetaungkyaw372005@gmail.com>
2024-01-11 18:10:16 +0630

    Initial commit

---
+++ src/main/java/com/example/hateoasspringdemo/HateoasSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hateoasspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasSpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HateoasSpringDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hateoasspringdemo/HateoasSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hateoasspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasSpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
